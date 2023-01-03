b77d8dd0e8216e94d2a4d0f44fdaaef14db8032b
LiXin <614401508@qq.com>
2023-01-04 04:10:33 +0800

    TEST

--- src/main/kotlin/com/katacr/kaguilds/NameIsOk.kt
+++ src/main/kotlin/com/katacr/kaguilds/NameIsOk.kt
@@ -2,7 +2,24 @@ package com.katacr.kaguilds
 
 
 fun checkName( name:String): Boolean {
-    var list = name.toCharArray()
-    println(list)
-    return true
+    var x = 0
+    var l = 0
+    for (i in name){
+        println(name[x])
+        x += 1
+        l += if (isAlp(name)){
+            1
+        } else
+            2
+    }
+    return l > 12
+
 }
+
+fun isAlp(string: String): Boolean {
+    val regex = """[a-zA-Z0-9_]+""".toRegex()
+    return regex.containsMatchIn(input = string)
+
+
+}
+
