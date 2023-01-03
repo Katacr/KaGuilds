124437543821812d47c2de0e3d4fe7dd6264ff7f
LiXin <614401508@qq.com>
2023-01-04 03:25:36 +0800

    TEST

--- src/main/kotlin/com/katacr/kaguilds/Command.kt
+++ src/main/kotlin/com/katacr/kaguilds/Command.kt
@@ -1,5 +1,6 @@
 package com.katacr.kaguilds
 
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import taboolib.common.LifeCycle
 import taboolib.common.platform.Awake
@@ -8,15 +9,17 @@ import taboolib.common.platform.command.command
 object Command {
     @Awake(LifeCycle.ENABLE)
     fun regCommand(){
+        // 注册指令头
         command(name = "KaGuilds", aliases = listOf("kg", "g", "guilds")){
+            //二级指令
             literal("create"){
-                dynamic() {
-                    execute<Player> {sender, context, _ ->
-                        var name = context.get(1)
-                        if (name.length > 6){
-                            sender.error("创建失败，您输入的长度过长。")
+                dynamic {
+                    execute<CommandSender> {sender, context, _ ->
+                        val name = context.get(1)
+                        if (!checkName(name)){
+                            sender.error("创建失败，您输入的名称长度过长。")
                         }else
-                            sender.tips("创建成功，您已成功创建${name}。")
+                            sender.tips("创建成功，您已成功创建[${name}]公会。")
 
                     }
                 }
---
+++ src/main/kotlin/com/katacr/kaguilds/NameIsOk.kt
@@ -0,0 +1,8 @@
+package com.katacr.kaguilds
+
+
+fun checkName( name:String): Boolean {
+    var list = name.toCharArray()
+    println(list)
+    return true
+}
