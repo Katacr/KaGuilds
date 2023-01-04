6722c0836eb7bdfd13e52a9ae0a5da1cb39b250b
LiXin <614401508@qq.com>
2023-01-04 23:54:22 +0800

    判断名称是否符合要求

--- src/main/kotlin/com/katacr/kaguilds/NameIsOk.kt
+++ src/main/kotlin/com/katacr/kaguilds/NameIsOk.kt
@@ -11,21 +11,21 @@ fun checkName(name: String, sender: Player) {
         var leng = 0
         for (i in name) {
             leng += if (isAlp(name[x].toString())) {
-                //若是则长度+1，否则长度+2
-                1
-            } else 2
+                //若是汉字则长度+2，否则长度+1
+                2
+            } else 1
             x += 1
         }
-        return if (leng < 11) {
+        return if (leng <= 12) {
             sender.tips("创建成功，您已成功创建[${name}]公会。")
         } else
             sender.error("创建失败，您输入的名称长度过长。")
     }
 }
 
-//遍历判断每个字符是否为字母数字
+//遍历判断每个字符是否为汉字
 fun isAlp(string: String): Boolean {
-    val regex = """[a-zA-Z0-9_]+""".toRegex()
+    val regex = """[\u4e00-\u9fa5]+""".toRegex()
     return regex.containsMatchIn(input = string)
 }
 
